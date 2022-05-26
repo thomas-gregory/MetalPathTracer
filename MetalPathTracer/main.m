216b2869236dc0b8e38f513953f7cc9f37986da4
Thomas Gregory <tomjosiahgregory@gmail.com>
2022-05-26 17:16:36 +0100

    Added main

--- MetalPathTracer/main.m
+++ MetalPathTracer/main.m
@@ -7,9 +7,6 @@
 
 #import <Cocoa/Cocoa.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-    }
+int main(int argc, const char* argv[]) {
     return NSApplicationMain(argc, argv);
 }
